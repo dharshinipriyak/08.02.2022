60952074e7477693e106af4b950a13c68cb31ffb
welcome <welcome@DESKTOP-2DL8QA7>
2022-03-25 19:00:12 +0530

    First commit

---
+++ 08.02.2022/src/main/java/com/mavenproject/Example1.java
@@ -0,0 +1,11 @@
+package com.mavenproject;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=15;
+		int b=22;
+		System.out.println(a+b);
+	}
+
+}
---
+++ 08.02.2022/src/main/java/com/mavenproject/Example2.java
@@ -0,0 +1,12 @@
+package com.mavenproject;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		int a=3, b=11,sum;
+		sum=a+b;
+		System.out.println("Sum of Numbers ="+sum);
+
+	}
+
+}
